56d4375240eff9d8352ce084352c766c5bdf8a4d
bbondalapati <131625154+bbondalapati@users.noreply.github.com>
2023-11-01 15:45:21 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are: 1.Python 2.Julia 3.R
+
+# Some of the commonly used libraries used by Data Scientists include: 1.Numpy 2.Pandas 3.Tensorflow
+
+# from IPython.display import Markdown, display
+# 
+# # Define the table contents
+# table = "| Tool | Description |\n| --- | --- |\n| NumPy | Numerical computing library |\n| Pandas | Data manipulation and analysis library |\n| Matplotlib | Data visualization library |\n| Scikit-learn | Machine learning library |\n| TensorFlow | Open-source machine learning platform |\n| Keras | High-level neural networks API |\n| PyTorch | Open-source machine learning library |\n| Seaborn | Data visualization library |\n| SciPy | Scientific computing library |\n| NLTK | Natural language processing library |\n| Statsmodels | Statistical modeling library |"
+# 
+# # Create the Markdown cell
+# display(Markdown(table))
+
+# ## Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# this a simple arithmetic expression to mutiply then add integers
+
+# In[4]:
+
+
+# Define the value in minutes
+minutes = 120
+
+# Convert minutes to hours
+hours = minutes / 60
+
+# Print the result
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# ## Author
+# Bhargav Bondalapati
+
+# In[ ]:
+
+
+
+
